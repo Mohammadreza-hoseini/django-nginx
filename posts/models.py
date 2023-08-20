3d663362de54432d0a1c0930ee813d6cc35f585b
mohammad-hoseini <mohammadrezajalahoseini@yahoo.com>
2023-08-20 16:51:29 +0330

    first commit

---
+++ posts/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField(max_length=500)
+    slug = models.SlugField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.body[:30]}'
+
+    class Meta:
+        ordering = ('-created',)
